e1b2e18c0dfe0954342ed2580e24fa33fc981731
lectorespa <lectorespa@gmail.com>
2026-09-18 18:45:33 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1120,7 +1120,7 @@ function inicializarEventos() {
       const response = await fetch(BACKEND_URL, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ input: plainText })
+        body: JSON.stringify({ textoPlano: plainText })
       });
 
       if (!response.ok) {
